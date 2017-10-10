deb4aa9979da5d22f731e80ddb5a974f14bf92ce
windmill mc <fujitsubo1324@gmail.com>
2017-10-10 22:12:48 +0900

    完成

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace HTMLSouceGetter
+{
+	class Program
+	{
+		static void Main()
+		{
+			WebClient webClient = new WebClient();
+			webClient.Encoding = Encoding.UTF8;
+			string inputedUrl = Console.ReadLine();
+			string targetSource = webClient.DownloadString(inputedUrl);
+			Console.Write(targetSource);
+			Console.ReadKey();
+			
+		}
+	}
+}
